fd3acb2f72c4cac01f2f5ac9a133d053b3f20a3c
edutjie <edutjitrahardja@gmail.com>
2021-08-14 23:33:27 +0700

    Form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,88 +6,239 @@ function Form() {
       <div class="max-w-xl mx-auto py-12 divide-y md:max-w-4xl">
         <div class="py-12">
           <h2 class="text-2xl font-bold">Formulir Pendaftaran Chronicles</h2>
-          <p class="mt-2 text-lg text-gray-600">
-            Unravel Your Legacy
-          </p>
+          <p class="my-2 text-lg text-gray-600">Unravel Your Legacy</p>
           <form action="#" method="POST">
-              <div className="shadow sm:rounded-md sm:overflow-hidden">
-                <div className="px-4 py-5 bg-white space-y-6 sm:p-6">
-                  <div className="grid grid-cols-3 gap-6">
-                    <div className="col-span-3 sm:col-span-2">
-                      
-                    </div>
+            <div className="shadow sm:rounded-md sm:overflow-hidden flex">
+              <div className="px-4 py-5 bg-white space-y-6 sm:p-6 w-full">
+                <div>
+                  <label
+                    htmlFor="asalsekolah"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Asal Sekolah
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      type="text"
+                      id="asalsekolah"
+                      name="asalsekolah"
+                      className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md"
+                      required
+                    />
                   </div>
+                </div>
+                <div>
+                  <label
+                    htmlFor="nomortelepon"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Nomor Telepon
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      type="number"
+                      id="nomortelepon"
+                      name="nomortelepon"
+                      className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md"
+                      required
+                    />
+                  </div>
+                </div>
 
-                  <div>
-                    <label htmlFor="about" className="block text-sm font-medium text-gray-700">
-                      About
+                <div>
+                  <label
+                    htmlFor="alamat"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Alamat
+                  </label>
+                  <div className="mt-1">
+                    <textarea
+                      id="alamat"
+                      name="alamat"
+                      rows={2}
+                      className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md"
+                      placeholder="12, Jl. Tanjung Duren Raya No.4"
+                      defaultValue={""}
+                      required
+                    />
+                  </div>
+                </div>
+                <div>
+                  <label
+                    htmlFor="provinsi"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Provinsi
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      type="text"
+                      id="provinsi"
+                      name="provinsi"
+                      className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md"
+                      required
+                    />
+                  </div>
+                </div>
+                <div>
+                  <label
+                    htmlFor="kecamatan"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Kecamatan
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      type="text"
+                      id="kecamatan"
+                      name="kecamatan"
+                      className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md"
+                      required
+                    />
+                  </div>
+                </div>
+                <div>
+                  <label
+                    htmlFor="kodepos"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Kode Pos
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      type="number"
+                      id="kodepos"
+                      name="kodepos"
+                      className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md"
+                      required
+                    />
+                  </div>
+                </div>
+                <div className="col-span-6 sm:col-span-3">
+                  <label
+                    htmlFor="jenjang"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Jenjang Sekolah
+                  </label>
+                  <select
+                    id="jenjang"
+                    name="jenjang"
+                    autoComplete="jenjang"
+                    className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    required
+                  >
+                    <option disabled selected value>
+                      Pilih Jenjang Sekolahmu
+                    </option>
+                    <option>SMA</option>
+                    <option>SMP</option>
+                  </select>
+                </div>
+              </div>
+              <div className="px-4 py-5 bg-white space-y-6 sm:p-6 w-full">
+                <div>
+                  <label
+                    htmlFor="contactperson"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Contact Person
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      type="text"
+                      id="contactperson"
+                      name="contactperson"
+                      className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md"
+                      required
+                    />
+                  </div>
+                </div>
+
+                <div className="grid grid-cols-3 gap-6">
+                  <div className="col-span-3">
+                    <label
+                      htmlFor="nomorteleponcp"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Nomor Telepon/HP
                     </label>
-                    <div className="mt-1">
-                      <textarea
-                        id="about"
-                        name="about"
-                        rows={3}
-                        className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md"
-                        placeholder="you@example.com"
-                        defaultValue={''}
-                      />
-                    </div>
-                    <p className="mt-2 text-sm text-gray-500">
-                      Brief description for your profile. URLs are hyperlinked.
-                    </p>
-                  </div>
-
-                  <div>
-                    <label className="block text-sm font-medium text-gray-700">Photo</label>
-                    <div className="mt-1 flex items-center">
-                      <span className="inline-block h-12 w-12 rounded-full overflow-hidden bg-gray-100">
-                        <svg className="h-full w-full text-gray-300" fill="currentColor" viewBox="0 0 24 24">
-                          <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
-                        </svg>
+                    <div className="mt-1 flex rounded-md shadow-sm">
+                      <span className="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 text-sm">
+                        +62
                       </span>
-                      <button
-                        type="button"
-                        className="ml-5 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                      >
-                        Change
-                      </button>
-                    </div>
-                  </div>
-
-                  <div>
-                    <label className="block text-sm font-medium text-gray-700">Cover photo</label>
-                    <div className="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
-                      <div className="space-y-1 text-center">
-                        <svg
-                          className="mx-auto h-12 w-12 text-gray-400"
-                          stroke="currentColor"
-                          fill="none"
-                          viewBox="0 0 48 48"
-                          aria-hidden="true"
-                        >
-                          <path
-                            d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-                            strokeWidth={2}
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                          />
-                        </svg>
-                        <div className="flex text-sm text-gray-600">
-                          <label
-                            htmlFor="file-upload"
-                            className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
-                          >
-                            <span>Upload a file</span>
-                            <input id="file-upload" name="file-upload" type="file" className="sr-only" />
-                          </label>
-                          <p className="pl-1">or drag and drop</p>
-                        </div>
-                        <p className="text-xs text-gray-500">PNG, JPG, GIF up to 10MB</p>
-                      </div>
+                      <input
+                        type="number"
+                        name="nomorteleponcp"
+                        id="nomorteleponcp"
+                        className="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300"
+                        placeholder="89999999"
+                        required
+                      />
                     </div>
                   </div>
                 </div>
-                <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
+
+                <div>
+                  <label
+                    htmlFor="cabanglomba"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Cabang Lomba
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      type="text"
+                      id="cabanglomba"
+                      name="cabanglomba"
+                      className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md"
+                      required
+                    />
+                  </div>
+                </div>
+
+                <div>
+                  <label
+                    htmlFor="pelatih"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Guru Pembimbing atau Pelatih
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      type="text"
+                      id="pelatih"
+                      name="pelatih"
+                      className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md"
+                    />
+                  </div>
+                </div>
+
+                <div className="grid grid-cols-3 gap-6">
+                  <div className="col-span-3">
+                    <label
+                      htmlFor="nomorteleponpelatih"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Nomor Telepon/HP
+                    </label>
+                    <div className="mt-1 flex rounded-md shadow-sm">
+                      <span className="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 text-sm">
+                        +62
+                      </span>
+                      <input
+                        type="number"
+                        name="nomorteleponpelatih"
+                        id="nomorteleponpelatih"
+                        className="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300"
+                        placeholder="89999999"
+                      />
+                    </div>
+                  </div>
+                </div>
+
+                <div className="px-4 py-3 text-right sm:px-6">
                   <button
                     type="submit"
                     className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
@@ -96,7 +247,8 @@ function Form() {
                   </button>
                 </div>
               </div>
-            </form>
+            </div>
+          </form>
         </div>
       </div>
     </div>
